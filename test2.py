be9f5d5c53a1d69b4e144cce70f1e3ab681ebce8
PurelyStupidity <micoolplays@gmail.com>
2023-02-14 02:11:09 +0000

    Feb 14, 2023, 3:11 PM

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 # url = ["https://www.youtube.com/watch?v=X4uHSpdZSdw", "https://stackoverflow.com/questions/48878855/python-base64-encoding-a-list"
-import itertools
 url_list = ["aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj10T1hvazZHZDQ2UQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vOVN6OUQ3ODNXQQ=="]
 import webbrowser as pygame
 import base64
-for _, url in itertools.product(range(5), url_list):
-    pygame.open_new(base64.b64decode(url))
+for _ in range(5):
+    for url in url_list:
+        pygame.open_new(base64.b64decode(url))
