4f40105fc324890795f2daebe239d0eadc47186e
PurelyStupidity <micoolplays@gmail.com>
2023-02-14 01:39:39 +0000

    Feb 14, 2023, 2:39 PM

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import os
 url = "https://www.youtube.com/watch?v=X4uHSpdZSdw"
 url = "https://google.com"
 for _ in range(10):
-    os.startfile(url, argument="--new-window")
+    os.startfile(url, arguments="--new-window")
