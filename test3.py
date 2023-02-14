bb118a4ef111eeffa1394b1c23ab4c037776c54f
PurelyStupidity <micoolplays@gmail.com>
2023-02-14 02:02:20 +0000

    Feb 14, 2023, 3:02 PM

--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@ import json
 print(list)
 import webbrowser as pygame
 import base64
-list = base64.b64encode(json.dumps(url)))
+list = base64.b64encode(json.dumps(url))
 print(list)
 for i in range(4):
     pygame.open_new(base64.b64decode(url))
