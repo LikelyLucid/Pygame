960f42b4234d3c65f5070d688da6f80df90c993a
PurelyStupidity <micoolplays@gmail.com>
2023-02-14 02:00:03 +0000

    Feb 14, 2023, 3:00 PM

---
+++ test3.py
@@ -0,0 +1,6 @@
+# url = ["https://www.youtube.com/watch?v=X4uHSpdZSdw", "https://stackoverflow.com/questions/48878855/python-base64-encoding-a-list"
+url = "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw=="
+import webbrowser as pygame
+import base64
+for i in range(4):
+    pygame.open_new(base64.b64decode(url))
