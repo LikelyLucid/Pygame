04c9196207cf366ef34ff0f984f0adfde5cb1ca2
PurelyStupidity <micoolplays@gmail.com>
2023-02-14 02:10:10 +0000

    Feb 14, 2023, 3:10 PM

--- test2.py
+++ test2.py
@@ -2,5 +2,5 @@
 url = ["aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj10T1hvazZHZDQ2UQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vOVN6OUQ3ODNXQQ=="]
 import webbrowser as pygame
 import base64
-for url in url):
+for url in url_list:
     pygame.open_new(base64.b64decode(url))
