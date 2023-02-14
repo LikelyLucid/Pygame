c4b9bffb68258225aa6c17b29fb84e1e1305f754
PurelyStupidity <micoolplays@gmail.com>
2023-02-14 01:54:39 +0000

    Feb 14, 2023, 2:54 PM

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 # url = "https://www.youtube.com/watch?v=X4uHSpdZSdw"
 url = "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw=="
-import webbrowser
+import webbrowser as 
 import base64
 for i in range(4):
     webbrowser.open_new(base64.b64decode(url))
