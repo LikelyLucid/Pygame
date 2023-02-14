d1e6365fb0cbba7e819197edfd284f14a2d40c80
PurelyStupidity <micoolplays@gmail.com>
2023-02-14 02:00:24 +0000

    Feb 14, 2023, 3:00 PM

--- test3.py
+++ test3.py
@@ -1,5 +1,5 @@
 url = ["https://www.youtube.com/watch?v=X4uHSpdZSdw", "https://stackoverflow.com/questions/48878855/python-base64-encoding-a-list"]
-
+list = base64.encode()
 import webbrowser as pygame
 import base64
 for i in range(4):
