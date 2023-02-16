400a058f38a10d6118757d8be6fa7c6323100360
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:20:05 +0000

    Feb 17, 2023, 11:20 AM

--- Component3.py
+++ Component3.py
@@ -24,6 +24,7 @@ quit_game = False
 snake_x = (screen_width-20) / 2
 snake_y = (screen_height-20) / 2
 
+snake_x_change = 0
 while not quit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
