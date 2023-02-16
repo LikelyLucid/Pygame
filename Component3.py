658408651cbc973f5b5594ff1ccb62f52c53ba97
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:20:56 +0000

    Feb 17, 2023, 11:20 AM

--- Component3.py
+++ Component3.py
@@ -33,6 +33,7 @@ while not quit_game:
         if event.type == pygame.QUIT:
             quit_game = True
         if event.type == pygame.KEYDOWN:
+            
     pygame.draw.rect(screen, red, (snake_x, snake_y, 20, 20))
     pygame.display.update()
 
