e047958c3a89f02a09b8076293dcd4aa22dab0f3
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:05:11 +0000

    Feb 17, 2023, 11:05 AM

--- Component2.py
+++ Component2.py
@@ -5,7 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 720))
 game_icon = pygame.image.load("icon.jpg")
 pygame.display.set_icon(game_icon)
-pygame.display.set_caption("Space Invaders")
+pygame.display.set_caption("Snake")
 time.sleep(5)
 
 pygame.quit()
