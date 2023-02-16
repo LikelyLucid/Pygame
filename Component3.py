8feb768d94bb54ed6e02c17ae5656991c58a30d7
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:34:24 +0000

    Feb 17, 2023, 11:34 AM

--- Component3.py
+++ Component3.py
@@ -9,8 +9,7 @@ msg_font = pygame.font.SysFont('franklingothicmediumcond', 20)
 def message(msg, txt_colour, bkgd_colour):
     txt = msg_font.render(msg, True, txt_colour, bkgd_colour)
     text_box = txt.get_rect(center=(screen_width/2, screen_height/2))
-
-    
+    screen.blit(txt, text_box)
 
 speed = 20
 screen_width = 1000
