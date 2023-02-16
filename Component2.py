9e03b8ef0bb2b103e353ef0d0dca477bca26aabb
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:13:45 +0000

    Feb 17, 2023, 11:13 AM

--- Component2.py
+++ Component2.py
@@ -26,7 +26,7 @@ while not quit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit_game = True
-    pygame.draw.rect(screen, white, (0, 0, screen_width, screen_height))
+    pygame.draw.rect(screen, red, (0, 0, screen_width, screen_height))
 
 pygame.quit()
 quit()
