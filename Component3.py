2e22aa0d3f813e68715897c0bd1abca96df7881d
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:40:35 +0000

    Feb 17, 2023, 11:40 AM

--- Component3.py
+++ Component3.py
@@ -68,8 +68,8 @@ while not quit_game:
     screen.fill(black)
     # pygame.draw.rect(screen, red, (snake_x, snake_y, 20, 20))
     # display game icon as snake
-    screen.blit(pygame.transform.scale(game_icon, (40, 40)), (snake_x, snake_y))
-    screen.blit(pygame.transform.scale(burger, (40, 40)), (food_x, food_y))
+    screen.blit(pygame.transform.scale(game_icon, (scale, scale)), (snake_x, snake_y))
+    screen.blit(pygame.transform.scale(burger, (scale, scale)), (food_x, food_y))
     pygame.display.update()
     clock.tick(5)
 
