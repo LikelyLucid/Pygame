01fe4a0e4669d2feda54bdb739d7d417b7fb1572
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:38:55 +0000

    Feb 17, 2023, 11:38 AM

--- Component3.py
+++ Component3.py
@@ -70,7 +70,7 @@ while not quit_game:
     # display game icon as snake
     screen.blit(pygame.transform.scale(game_icon, (100, 100)), (snake_x, snake_y))
     pygame.display.update()
-    screen.blit
+    screen.blit()
     clock.tick(5)
 
 message("You died", black, white)
