ea3640003d1364b6aa3f8277ca3e6f71315f86a7
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:51:29 +0000

    Feb 17, 2023, 11:51 AM

--- Component3.py
+++ Component3.py
@@ -81,11 +81,5 @@ while not quit_game:
 message("You died", black, white)
 pygame.display.update()
 time.sleep(3)
-url_list = ["aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj10T1hvazZHZDQ2UQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vOVN6OUQ3ODNXQQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw=="]
-import webbrowser as pygame
-import base64
-for _ in range(5):
-    for url in url_list:
-        pygame.open_new(base64.b64decode(url))
 pygame.quit()
 quit()
