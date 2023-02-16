6f20ef0551a65c7b419e60e85c14e79049f2ee3b
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:38:08 +0000

    Feb 17, 2023, 11:38 AM

--- Component3.py
+++ Component3.py
@@ -39,7 +39,7 @@ snake_y = (screen_height-20) / 2
 snake_x_change = 0
 snake_y_change = 0
 
-food_x = round(random.randrange(20))
+food_x = round(random.randrange(20,))
 while not quit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
