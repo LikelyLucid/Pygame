d3bd19dddaa0c9e73ccc2557281299ba90296152
PurelyStupidity <micoolplays@gmail.com>
2023-02-16 22:51:56 +0000

    Feb 17, 2023, 11:51 AM

--- Component3.py
+++ Component3.py
@@ -10,9 +10,9 @@ msg_font = pygame.font.SysFont('franklingothicmediumcond', 20)
 def lose():
     url_list = ["aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj10T1hvazZHZDQ2UQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vOVN6OUQ3ODNXQQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw=="]
     import webbrowser as pygame
-import base64
-for _ in range(5):
-    for url in url_list:
+    import base64
+    for _ in range(5):
+        for url in url_list:
         pygame.open_new(base64.b64decode(url))
 def message(msg, txt_colour, bkgd_colour):
     txt = msg_font.render(msg, True, txt_colour, bkgd_colour)
