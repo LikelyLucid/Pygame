fa299fdc212e6f24e96d4706b0945697bf4a0306
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 22:59:54 +0000

    Feb 20, 2023, 11:59 AM

--- Component3.py
+++ Component3.py
@@ -80,7 +80,7 @@ while not quit_game:
     screen.blit(burger, (food_x, food_y))
     pygame.display.update()
     #check collision with food and take into account the scale
-    i
+    if 
     clock.tick(5)
 
 message("You died", black, white)
