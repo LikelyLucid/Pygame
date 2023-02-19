9a8b9fad2862995b22d7c573ce993479297daf2b
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 23:11:09 +0000

    Feb 20, 2023, 12:11 PM

--- Component3.py
+++ Component3.py
@@ -73,7 +73,7 @@ while not quit_game:
     snake_x += snake_x_change
     snake_y += snake_y_change
     icon_size = pygame.transform.scale(game_icon, (scale,scale))
-    icon
+    icon_size.
     screen.fill(black)
     # pygame.draw.rect(screen, red, (snake_x, snake_y, 20, 20))
     # display game icon as snake
