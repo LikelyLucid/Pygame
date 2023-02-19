1d2cfc6ca70b9cb49484a57cead8862fa0306adc
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 23:21:36 +0000

    Feb 20, 2023, 12:21 PM

--- Component3.py
+++ Component3.py
@@ -88,7 +88,8 @@ while not quit_game:
         obsticles.append((obsticle_x, obsticle_y))
 
     # setup collison so if the player is touching the green obsticles then they lose
-    if snake_ x 
+    for obsticle in obsticles:
+        
     screen.blit(pygame.transform.scale(game_icon, (scale, scale)), (snake_x, snake_y))
     screen.blit(burger, (food_x, food_y))
     for obsticle in obsticles:
