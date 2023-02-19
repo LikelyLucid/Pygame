115be4b270ef31ed5ab7fc31bb4c24e32f821fc5
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 23:40:41 +0000

    Feb 20, 2023, 12:40 PM

--- Component3.py
+++ Component3.py
@@ -1,11 +1,12 @@
 import pygame
 import time
 import random
+
 pygame.init()
 
-score_font = pygame.font.SysFont('arialblack', 20)
-exit_font = pygame.font.SysFont('arialblack', 30)
-msg_font = pygame.font.SysFont('franklingothicmediumcond', 20)
+score_font = pygame.font.SysFont("arialblack", 20)
+exit_font = pygame.font.SysFont("arialblack", 30)
+msg_font = pygame.font.SysFont("franklingothicmediumcond", 20)
 
 obsticle_size = 10
 obsticles = []
@@ -19,23 +20,39 @@ screen_height = 720
 screen_border = 20
 
 touching_frame = 0
+
+
 def lose():
-    url_list = ["aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj10T1hvazZHZDQ2UQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vOVN6OUQ3ODNXQQ==", "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw=="]
+    url_list = [
+        "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj10T1hvazZHZDQ2UQ==",
+        "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vOVN6OUQ3ODNXQQ==",
+        "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1YNHVIU3BkWlNkdw==",
+    ]
     import webbrowser as pygame
     import base64
+
     for _ in range(5):
         for url in url_list:
             pygame.open_new(base64.b64decode(url))
 
+
 def message(msg, txt_colour, bkgd_colour):
     txt = msg_font.render(msg, True, txt_colour, bkgd_colour)
-    text_box = txt.get_rect(center=(screen_width/2, screen_height/2))
+    text_box = txt.get_rect(center=(screen_width / 2, screen_height / 2))
     screen.blit(txt, text_box)
 
+
 def add_obsticle():
-    obsticle_x = round(random.randrange(screen_border, screen_width-screen_border) / 20.0) * 20.0
-    obsticle_y = round(random.randrange(screen_border, screen_height-screen_border) / 20.0) * 20.0
+    obsticle_x = (
+        round(random.randrange(screen_border, screen_width - screen_border) / 20.0)
+        * 20.0
+    )
+    obsticle_y = (
+        round(random.randrange(screen_border, screen_height - screen_border) / 20.0)
+        * 20.0
+    )
     obsticles.append((obsticle_x, obsticle_y))
+
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
@@ -55,14 +72,18 @@ clock = pygame.time.Clock()
 
 quit_game = False
 
-snake_x = (screen_width-20) / 2
-snake_y = (screen_height-20) / 2
+snake_x = (screen_width - 20) / 2
+snake_y = (screen_height - 20) / 2
 
 snake_x_change = 0
 snake_y_change = 0
 
-food_x = round(random.randrange(screen_border, screen_width-screen_border) / 20.0) * 20.0
-food_y = round(random.randrange(screen_border, screen_height-screen_border) / 20.0) * 20.0
+food_x = (
+    round(random.randrange(screen_border, screen_width - screen_border) / 20.0) * 20.0
+)
+food_y = (
+    round(random.randrange(screen_border, screen_height - screen_border) / 20.0) * 20.0
+)
 
 while not quit_game:
     for event in pygame.event.get():
@@ -82,18 +103,29 @@ while not quit_game:
                 snake_y_change = speed
                 snake_x_change = 0
 
-    if snake_x >= screen_width or snake_x < 0 or snake_y >= screen_height or snake_y < 0:
+    if (
+        snake_x >= screen_width
+        or snake_x < 0
+        or snake_y >= screen_height
+        or snake_y < 0
+    ):
         quit_game = True
     snake_x += snake_x_change
     snake_y += snake_y_change
-    icon_size = pygame.transform.scale(game_icon, (scale,scale))
+    icon_size = pygame.transform.scale(game_icon, (scale, scale))
     screen.fill(black)
     # pygame.draw.rect(screen, red, (snake_x, snake_y, 20, 20))
     # display game icon as snake
     food_rect = pygame.Rect(food_x, food_y, burger.get_width(), burger.get_height())
     if food_rect.colliderect(pygame.Rect(snake_x, snake_y, scale, scale)):
-        food_x = round(random.randrange(screen_border, screen_width-screen_border) / 20.0) * 20.0
-        food_y = round(random.randrange(screen_border, screen_height-screen_border) / 20.0) * 20.0
+        food_x = (
+            round(random.randrange(screen_border, screen_width - screen_border) / 20.0)
+            * 20.0
+        )
+        food_y = (
+            round(random.randrange(screen_border, screen_height - screen_border) / 20.0)
+            * 20.0
+        )
         scale += 10
         add_obsticle()
 
@@ -102,8 +134,12 @@ while not quit_game:
     for obsticle in obsticles:
         if buffer_number != touching_frame:
             continue
-        obsticle_rect = pygame.Rect(obsticle[0], obsticle[1], obsticle_size, obsticle_size)
-        if obsticle_rect.colliderect(pygame.Rect(snake_x, snake_y, icon_size.get_width(), icon_size.get_height())):
+        obsticle_rect = pygame.Rect(
+            obsticle[0], obsticle[1], obsticle_size, obsticle_size
+        )
+        if obsticle_rect.colliderect(
+            pygame.Rect(snake_x, snake_y, icon_size.get_width(), icon_size.get_height())
+        ):
             touching_frame += 1
     if buffer_number == touching_frame:
         touching_frame = 0
@@ -112,9 +148,11 @@ while not quit_game:
     screen.blit(pygame.transform.scale(game_icon, (scale, scale)), (snake_x, snake_y))
     screen.blit(burger, (food_x, food_y))
     for obsticle in obsticles:
-        pygame.draw.rect(screen, green, (obsticle[0], obsticle[1], obsticle_size, obsticle_size))
+        pygame.draw.rect(
+            screen, green, (obsticle[0], obsticle[1], obsticle_size, obsticle_size)
+        )
     pygame.display.update()
-    #check collision with food and take into account the scale
+    # check collision with food and take into account the scale
 
     clock.tick(5)
 
