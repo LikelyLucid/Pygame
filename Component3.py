baa6b9b04fad315e11d50a40ffd6cf10b100b358
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 23:40:38 +0000

    Feb 20, 2023, 12:40 PM

--- Component3.py
+++ Component3.py
@@ -106,7 +106,7 @@ while not quit_game:
         if obsticle_rect.colliderect(pygame.Rect(snake_x, snake_y, icon_size.get_width(), icon_size.get_height())):
             touching_frame += 1
     if buffer_number == touching_frame:
-        
+        touching_frame = 0
     if touching_frame > 10:
         quit_game = True
     screen.blit(pygame.transform.scale(game_icon, (scale, scale)), (snake_x, snake_y))
