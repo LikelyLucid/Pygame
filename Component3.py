1a679e18222f488ba057e34384d4886cffbeddfe
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 23:00:10 +0000

    Feb 20, 2023, 12:00 PM

--- Component3.py
+++ Component3.py
@@ -83,7 +83,7 @@ while not quit_game:
     if abs(snake_x - food_x) < scale and abs(snake_y - food_y) < scale:
         food_x = round(random.randrange(20, screen_width-20) / 20.0) * 20.0
         food_y = round(random.randrange(20, screen_height-20) / 20.0) * 20.0
-        score += 1
+        scale += 1
     clock.tick(5)
 
 message("You died", black, white)
