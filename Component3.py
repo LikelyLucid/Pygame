841d42114861650393904227babc102d7b8ce558
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 23:39:42 +0000

    Feb 20, 2023, 12:39 PM

--- Component3.py
+++ Component3.py
@@ -105,7 +105,7 @@ while not quit_game:
         obsticle_rect = pygame.Rect(obsticle[0], obsticle[1], obsticle_size, obsticle_size)
         if obsticle_rect.colliderect(pygame.Rect(snake_x, snake_y, icon_size.get_width(), icon_size.get_height())):
             touching_frame += 1
-    if touching
+    if touching_frame 
     screen.blit(pygame.transform.scale(game_icon, (scale, scale)), (snake_x, snake_y))
     screen.blit(burger, (food_x, food_y))
     for obsticle in obsticles:
