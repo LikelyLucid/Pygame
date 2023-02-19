f55a364c84083097650d0a298183995f9bc4429a
PurelyStupidity <micoolplays@gmail.com>
2023-02-19 23:20:32 +0000

    Feb 20, 2023, 12:20 PM

--- Component3.py
+++ Component3.py
@@ -87,7 +87,7 @@ while not quit_game:
         obsticle_y = round(random.randrange(20, screen_height-20) / 20.0) * 20.0
         obsticles.append((obsticle_x, obsticle_y))
 
-    if 
+    #setup collison
     screen.blit(pygame.transform.scale(game_icon, (scale, scale)), (snake_x, snake_y))
     screen.blit(burger, (food_x, food_y))
     for obsticle in obsticles:
