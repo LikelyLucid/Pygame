919fb5a23e256491141b97db832ee9d6ee656b67
PurelyStupidity <micoolplays@gmail.com>
2023-02-20 22:03:14 +0000

    Feb 21, 2023, 11:03 AM

--- Component3.py
+++ Component3.py
@@ -143,6 +143,7 @@ while not quit_game:
             touching_frame += 1
     if buffer_number == touching_frame:
         touching_frame = 0
+    
     if touching_frame > 10:
         quit_game = True
     screen.blit(pygame.transform.scale(game_icon, (scale, scale)), (snake_x, snake_y))
