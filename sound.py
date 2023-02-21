39da4370fccf4e09eb3fc369adad92cd77bc4364
PurelyStupidity <micoolplays@gmail.com>
2023-02-21 01:33:48 +0000

    Feb 21, 2023, 2:33 PM

--- sound.py
+++ sound.py
@@ -1,6 +1,3 @@
-import pygame
-from pygame import mixer
-import random
-mixer.init()
-# play biting1.wav
-mixer.Sound.play(mixer.Sound("./biting3.wav"))
+number = random.randint(1, 6)
+
+    playsound.playsound("./biting" + str(number) + ".wav")
